7683af14d00f0c2fce3b2767034781b5fb52af82
Toni Horne <tonihorne@Tonis-Air.fios-router.home>
2020-02-29 16:18:28 -0500

    initialized state in Signup

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,66 @@
+import React, { Component} from 'react';
+
+
+
+class SignupForm extends Component  {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirmation: ''
+    };
+
+   handleChange = e => {
+       this.setState({
+           [e.target.name]: e.target.value
+       });
+
+   } 
+
+    render () {
+        return (
+            <form>  
+                <fieldset> 
+                    <legend>Sign Up</legend>
+                        <label htmlFor="name">Full Name</label>
+                        <input 
+                            id="name" 
+                            name="name" 
+                            type="text" 
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            id="email" 
+                            name="email" 
+                            type="email" 
+                            value={this.state.email} 
+                            onChange={this.handleChange}
+                        />
+
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            name="password"     
+                            type="password" 
+                            value={this.state.password} 
+                            onChange={this.handleChange}                        />
+
+                        <label htmlFor="passwordConfirmation">Confirm Password</label>
+                        <input 
+                            name="passwordConfirmation" 
+                            type="password" 
+                            value={this.state.passwordConfirmation} 
+                            onChange={this.handleChange}
+                        />
+
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
+        );
+    }
+}
+
+export default SignupForm;
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import SignupForm from '../../components/SignupForm/SignupForm';
 
 const Signup= (props) => {
     return(
+        <main>
         <h1>Signup  </h1>
+        <SignupForm />
+        </main>
     );
 };
 
