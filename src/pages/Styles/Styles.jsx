4b59be66356ff2b777108addd415c88c78720c50
Toni Horne <tonihorne@Tonis-Air.fios-router.home>
2020-03-05 23:05:05 -0500

    added styles jsx and module

---
+++ src/pages/Styles/Styles.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styles from './Styles/Styles';
+
+const Styles= (props) => {
+    return(
+        <main>
+        <h1>Styles</h1>
+       
+        </main>
+    );
+};
+
+export default Styles;
