f84f3092faaec2f6fb48631858f6d90d59c9d19b
Toni Horne <tonihorne@Tonis-Air.fios-router.home>
2020-03-06 19:21:27 -0500

    new style logic

--- src/components/OotdLogic/OotdLogic.js
+++ src/components/OotdLogic/OotdLogic.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import getWeatherData from '../../utils/getWeatherData';
 
 
-function OotdLogic(props) {
-    const warmWeather = props.getWeatherData;
- 
-    {Math.trunc(weatherData.main.temp)> 40} 
-    return <h1>hey its hot, wear boots</h1>
+const OotdLogic= (props) => {
+
+    return <h1>hey girl its hot</h1>
 }
-
 
 export default OotdLogic;
